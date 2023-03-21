f18c5b6acfc4d766cb1e5b406bc87d9e2c95e83d
Luis Florez <braian1k3@gmail.com>
2023-03-21 18:29:48 -0500

    Create proyect with help of Spring Initializr.

---
+++ src/main/java/com/example/SpringBootBackpack/SpringBootBackpackApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootBackpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackpackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackpackApplication.class, args);
+	}
+
+}

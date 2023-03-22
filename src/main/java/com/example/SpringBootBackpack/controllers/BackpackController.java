5f003c4dc7f3b1e695555da69f7262a62801edce
Luis Florez <braian1k3@gmail.com>
2023-03-21 19:30:40 -0500

    Generate class and controller for each entity.

---
+++ src/main/java/com/example/SpringBootBackpack/controllers/BackpackController.java
@@ -0,0 +1,4 @@
+package com.example.SpringBootBackpack.controllers;
+
+public class BackpackController {
+}
---
+++ src/main/java/com/example/SpringBootBackpack/entitys/Objectt.java
@@ -0,0 +1,4 @@
+package com.example.SpringBootBackpack.entitys;
+
+public class Objectt {
+}
